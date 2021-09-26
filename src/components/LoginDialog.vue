7b5037f12f29a2f53b37ca005ac7d4d1a07f7595
jasonfritsche <fritsche.jason@gmail.com>
2021-09-25 21:58:21 -0500

    login style wip

---
+++ src/components/LoginDialog.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    class="
+      grid grid-cols-1
+      bg-blue-600
+      w-11/12
+      md:w-9/12
+      lg:w-5/12
+      rounded-md
+      pb-6
+      pt-1
+      pl-3
+      pr-3
+    "
+  >
+    <div>{{ headerText }}</div>
+    <div class="flex flex-col">
+      <div class="p-3">
+        <input type="text" />
+      </div>
+      <div class="p-3">
+        <input type="text" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    headerText: String
+  },
+  setup() {}
+})
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Signup</div>
+  <login-dialog headerText="Sign Up" />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import LoginDialog from '../components/LoginDialog.vue'
+
+export default defineComponent({
+  components: { LoginDialog },
+  setup() {}
+})
+</script>
